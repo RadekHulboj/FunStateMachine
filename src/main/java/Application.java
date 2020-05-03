d3522eeff4b26a4a01857a122153548085e90d7f
Radoslaw Hulboj <radoslaw.hulboj@capgemini.com>
2020-05-03 19:45:29 +0200

    clean code

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,8 +5,8 @@ public class Application {
     public static void main(String[] args) {
         StateMachineBuilder stateMachineBuilder = new StateMachineBuilder();
         IStateMachine<StateMachineBuilder.States, StateMachineBuilder.Events> IStateMachine = stateMachineBuilder::build;
-        //StateMachineBuilder.States state = IStateMachine.getState(StateMachineBuilder.Events.EV_WORK, "Ala");
-        StateMachineBuilder.States state = IStateMachine.getState(StateMachineBuilder.Events.EV_WORK);
+        StateMachineBuilder.States state = IStateMachine.getState(StateMachineBuilder.Events.EV_WORK, "Ala");
+        //StateMachineBuilder.States state = IStateMachine.getState(StateMachineBuilder.Events.EV_WORK);
     }
 
 
